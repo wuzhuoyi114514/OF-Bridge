86d0cdf87339941242ea312e68b082847c38c901
wuzhuoyi114514 <111749626+wuzhuoyi114514@users.noreply.github.com>
2022-12-24 14:52:46 +0800

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using System.Windows;
+using System.Windows.Resources;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyAssociatedContentFile("appicon.ico")]
+[assembly: AssemblyCompany("Of Bridge")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("Of Bridge")]
+[assembly: AssemblyTitle("Of Bridge")]
+[assembly: AssemblyVersion("1.0.0.0")]
